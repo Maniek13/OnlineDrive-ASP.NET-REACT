a7764af960ef71bfdcc4bec1975d571d3e15c7fe
Maniek13 <mani3k1989@gmail.com>
2021-06-13 17:51:14 +0200

    ITree

--- TreeExplorer/Interfaces/ITree.cs
+++ TreeExplorer/Interfaces/ITree.cs
@@ -6,11 +6,11 @@ namespace TreeExplorer.Interfaces
 {
     interface ITree
     {
-        static bool Add(int id, string name, string type, int idW) => throw new NotImplementedException();
-        static bool Edit(Element element) => throw new NotImplementedException();
-        static bool Delete(int id) => throw new NotImplementedException();
-        static bool Move(int id, int idW) => throw new NotImplementedException();
-        static IEnumerable<Element> Sort(int idW, string type) => throw new NotImplementedException();
+        virtual bool Add(int id, string name, string type, int idW) => throw new NotImplementedException();
+        virtual bool Edit(Element element) => throw new NotImplementedException();
+        virtual bool Delete(int id) => throw new NotImplementedException();
+        virtual bool Move(int id, int idW) => throw new NotImplementedException();
+        virtual IEnumerable<Element> Sort(int idW, string type) => throw new NotImplementedException();
         List<Element> Show();
     }
 }
