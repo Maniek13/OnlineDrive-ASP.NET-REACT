7f2f2f2e32af66f380e6c525fb33b70cf033d42b
Maniek13 <mani3k1989@gmail.com>
2021-06-14 06:32:37 +0200

    add

--- Fronted/tree-explorer/src/elements/tree/forms/add_form.js
+++ Fronted/tree-explorer/src/elements/tree/forms/add_form.js
@@ -31,15 +31,17 @@ class AddForm extends React.Component{
         return (
         <React.Fragment>
             <div className={styles.add_form}>
-                <label>Name:</label>
-                <input id="name" type="text" onChange={this.name.bind(this)}/>
-                <label>Is folder?</label> 
-                <input value={this.state.checked} type="checkbox" id="type"  defaultChecked={false} onChange={this.type.bind(this)}/>
+              <div className={styles.el_form}>
+                 <label className={styles.label}>Name:</label>
+                <input id="name" type="text" onChange={this.name.bind(this)} className={styles.input}/>
+              </div>
+              <div className={styles.el_form}>
+                <label className={styles.label}>Is folder?</label> 
+                <input value={this.state.checked} type="checkbox" id="type"  defaultChecked={false} onChange={this.type.bind(this)} className={styles.input}/>
+              </div>
+              <button className={styles.add_form_btn} onClick={this.add}>Add</button>
             </div>
-
-            <button className={styles.add_btn} onClick={this.add}></button>
         </React.Fragment>
-        
         );
     }
 }
