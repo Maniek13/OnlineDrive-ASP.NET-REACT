dda17703f380acefe8e332652e72adc1e7c8a576
Maniek13 <mani3k1989@gmail.com>
2021-06-15 23:17:37 +0200

    end

--- Fronted/tree-explorer/src/elements/tree/elements/folder.js
+++ Fronted/tree-explorer/src/elements/tree/elements/folder.js
@@ -102,6 +102,7 @@ class Folder extends React.Component {
           <div className={styles.folder_contener}>
             <div className={styles.folder} >
               <a className={styles.label}>{this.props.name}</a>
+              <button id={this.props.id}  name={this.props.name} className={styles.del_btn} onClick={this.delForm.bind(this)}>x</button>
               <button className={styles.show_btn} onClick={this.showBranch.bind(this)}>&lsaquo;&rsaquo;</button>  
               <button className={styles.show_btn} onClick={this.sortBranch.bind(this)}>&uarr;&darr;</button>  
               <button id={this.props.id} idW={this.props.idW} name={this.props.name} fileType={this.props.fileType}  className={styles.edit_btn} onClick={this.editForm.bind(this)}></button> 
