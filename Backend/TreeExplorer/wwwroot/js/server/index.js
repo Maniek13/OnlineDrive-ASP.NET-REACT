dcecd364d3c90cb4c06fcc0edfd5f44a7331f12c
Maniek13 <mani3k1989@gmail.com>
2021-08-25 03:00:10 +0200

    add index.js

---
+++ Backend/TreeExplorer/wwwroot/js/server/index.js
@@ -0,0 +1,13 @@
+﻿function set() {
+    fetch('https://localhost:5001/Elements/Set')
+        .then(response => response.json())
+        .then(data => {
+            if (data.ok === true) {
+                document.getElementById("responde").innerText = "Data is set";
+                document.getElementById("set_button").disabled = true;
+            }
+            else {
+                document.getElementById("responde").innerText = "Server not responde";
+            }
+        });
+}
