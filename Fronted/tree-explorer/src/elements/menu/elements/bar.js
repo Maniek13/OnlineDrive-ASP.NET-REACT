56fb4fb3c10496f23c80188c3b4c17d6b0ecf2b8
Maniek13 <mani3k1989@gmail.com>
2021-08-26 15:14:40 +0200

    CLEAN CODE

--- Fronted/tree-explorer/src/elements/menu/elements/bar.js
+++ Fronted/tree-explorer/src/elements/menu/elements/bar.js
@@ -1,26 +1,21 @@
 import React from 'react'
 import AccountController from '../../../controllers/account/account_controller'
-import Responde from '../../../objects/responde';
 import Usser from '../../account_login/objects/usser';
 import styles from '../styles/menu.module.css'
 
 class Bar extends React.Component {
-    constructor(props){
-        super(props)
-    }
-
     async logout(){
         await AccountController.remove_data();
         window.location.reload();
     }
 
     render() {
-    return (
-        <div className={styles.bar}>
-            <div className={styles.name}>{Usser.usser.Name}</div>
-            <button className={styles.logout_btn} onClick={this.logout}>LogOut</button>
-        </div>
-    );
+        return (
+            <div className={styles.bar}>
+                <div className={styles.name}>{Usser.usser.Name}</div>
+                <button className={styles.logout_btn} onClick={this.logout}>LogOut</button>
+            </div>
+        );
     }
 }
 
