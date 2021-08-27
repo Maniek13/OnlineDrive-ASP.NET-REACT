7339ecd9a1c65acc465835d29de6ef54e4578fc4
Maniek13 <mani3k1989@gmail.com>
2021-08-27 11:13:56 +0200

    login form css

--- Fronted/tree-explorer/src/controllers/file/file_controller.js
+++ Fronted/tree-explorer/src/controllers/file/file_controller.js
@@ -1,14 +1,10 @@
-import POST from '../http/post'
-import File from '../http/file';
-import Element from '../../elements/tree/objects/element';
-import Responde from '../../objects/responde';
-
+import File from '../http/file'
+import Element from '../../elements/tree/objects/element'
 
 class FileController{
     static async download(){
         await File("https://localhost:5001/Elements/GetFile", {Id : Element.element.Id});
     }
-
 }
 
 export default FileController;
--- Fronted/tree-explorer/src/elements/account_login/elements/form.js
+++ Fronted/tree-explorer/src/elements/account_login/elements/form.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import AccountController from '../../../controllers/account/account_controller';
-import FileController from '../../../controllers/file/file_controller';
-import Responde from '../../../objects/responde';
-import Usser from '../objects/usser';
+import AccountController from '../../../controllers/account/account_controller'
+import Responde from '../../../objects/responde'
+import Usser from '../objects/usser'
 import styles from '../styles/account.module.css'
-import Error from './error';
+import Error from './error'
 
 class Form extends React.Component {
   constructor(props){
@@ -62,15 +61,22 @@ class Form extends React.Component {
   render() {
     return (
       <div className={styles.login_form}>
+        <p>Login to online drive</p>
+        <div className={styles.input_container}> 
           <input className={styles.input_form} id="login" placeholder="Login" onChange={this.login_data.bind(this)}></input>
           <input className={styles.input_form} id="password" placeholder="Password" onChange={this.password_data.bind(this)}></input>
-          <div className={styles.remember_me_form}>
-                <label>Remember me</label> 
-                <input value={this.state.remember} type="checkbox" id="type"  defaultChecked={false} onChange={this.remember.bind(this)} className={styles.input}/>
-              </div>
+        </div>
+          
+        <div className={styles.remember_me_form}>
+              <label>Remember me</label> 
+              <input value={this.state.remember} type="checkbox" id="type"  defaultChecked={false} onChange={this.remember.bind(this)} className={styles.input}/>
+        </div>
+        <div className={styles.btn_container}>
           <button className={styles.btn} onClick={this.login.bind(this)}>Login</button>
           <button className={styles.btn} onClick={this.register.bind(this)}>Register</button>
-          {this.state.error ? <Error/> : ""}
+        </div>
+        
+        {this.state.error ? <Error/> : ""}
       </div>
     );
   }
